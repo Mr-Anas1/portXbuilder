d9c8877394c782faa9151d88f83a8d86389ae62c
Mr-Anas1 <a3689211@gmail.com>
2025-06-22 10:24:40 +0530

    fixed some bugs

--- src/middleware.js
+++ src/middleware.js
@@ -1,6 +1,31 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-export default clerkMiddleware();
+// List of API routes that don't need authentication
+const publicApiRoutes = [
+  "/api/verify-payment",
+  "/api/webhooks",
+  "/api/update-plan",
+];
+
+function customMiddleware(request) {
+  const { pathname } = request.nextUrl;
+
+  // Check if this is a public API route
+  const isPublicApiRoute = publicApiRoutes.some((route) =>
+    pathname.startsWith(route)
+  );
+
+  // If it's a public API route, skip authentication
+  if (isPublicApiRoute) {
+    return NextResponse.next();
+  }
+
+  // Otherwise, apply Clerk authentication
+  return clerkMiddleware()(request);
+}
+
+export default customMiddleware;
 
 export const config = {
   matcher: [
